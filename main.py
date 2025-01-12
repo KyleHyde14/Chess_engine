2c1d0fead832ee4bd90f51a10eb6ca3cba58afc2
KyleHyde14 <jesushdezjob@gmail.com>
2025-01-12 23:40:27 +0100

    added pieces to board

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import pygame
 
-# Configuración inicial
-WINDOW_SIZE = 600  # Tamaño de la ventana (600x600 píxeles)
-BOARD_SIZE = 8     # Tamaño del tablero (8x8)
-CELL_SIZE = WINDOW_SIZE // BOARD_SIZE  # Tamaño de cada celda
+WINDOW_SIZE = 512  
+BOARD_SIZE = 8     
+CELL_SIZE = WINDOW_SIZE // BOARD_SIZE  
 
 # Colores
 WHITE = (222,184,135)
 BLACK = (79,48,31)
 
+START_POSITIONS = [
+    ["R", "H", "B", "Q", "K", "B", "H", "R"],
+    ["P", "P", "P", "P", "P", "P", "P", "P"],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    ["P", "P", "P", "P", "P", "P", "P", "P"],
+    ["R", "H", "B", "Q", "K", "B", "H", "R"]
+]
+
 def draw_board(screen):
-    """Dibuja el tablero de ajedrez en la pantalla."""
     for row in range(BOARD_SIZE):
         for col in range(BOARD_SIZE):
             color = WHITE if (row + col) % 2 == 0 else BLACK
             rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(screen, color, rect)
 
+def draw_pieces(screen, pieces, position):
+    for row in range(BOARD_SIZE):
+        for col in range(BOARD_SIZE):
+            piece = START_POSITIONS[row][col]
+            if piece != ' ':
+                piece_color = 'white' if row >= 6 else 'black'
+                piece_img = pygame.image.load(f'images/{piece_color}_{piece}.png')
+                screen.blit(piece_img, (col * CELL_SIZE, row * CELL_SIZE))
+
 def main():
-    # Inicializa Pygame
     pygame.init()
     screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
-    pygame.display.set_caption("Tablero de Ajedrez")
+    pygame.display.set_caption("Chess Baby")
     clock = pygame.time.Clock()
     running = True
 
@@ -30,10 +47,9 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        # Dibujar tablero
         draw_board(screen)
+        draw_pieces(screen, 0, (0, 0))
 
-        # Actualizar la pantalla
         pygame.display.flip()
         clock.tick(60)
 
